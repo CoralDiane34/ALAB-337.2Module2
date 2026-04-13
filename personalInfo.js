7bb03f3cd5e5c57d0dd50d1568400c83caf6ab2b
CoralDiane34 <siouxperflychick@gmail.com>
2026-04-13 10:27:22 -0500

    Edited javascript file

---
+++ personalInfo.js
@@ -0,0 +1,6 @@
+let name1 = "Coral Langdeau"; // This is my married name.
+let message = "I am " + 39 + " years old.";
+let height = 65; // in inches
+let isStudent = true;
+
+console.log("Hello, my name is " + name1 + ". " + message + " I am " + height + " inches tall, and it is " + isStudent + " that I am a learner.");
